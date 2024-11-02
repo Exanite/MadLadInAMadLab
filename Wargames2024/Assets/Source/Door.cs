37ce83f81ec1068ba1a0fa88a3e461ebe1b8416b
Exanite <exanite78012@gmail.com>
2024-11-02 00:25:09 -0400

    Implement WireNetwork system for Door and PressurePlate

--- Wargames2024/Assets/Source/Door.cs
+++ Wargames2024/Assets/Source/Door.cs
@@ -1,40 +1,28 @@
 using UnityEngine;
-using Exanite.Core.Utilities;
-using UnityEngine.InputSystem.Controls;
-using System;
 
-
-public class Door : MonoBehaviour{
+public class Door : MonoBehaviour
+{
     [Header("Dependencies")]
     public Sprite[] Sprites;
     public SpriteRenderer SpriteRenderer;
     public Rigidbody2D rigidbody2D;
-    // Update is called once per frame
-    float i = 0;
-    bool open = false;
+    public WireNetwork Network;
+
+    public float speed = 1;
     public bool shouldOpen = false;
-    public bool shouldChange = false;
-    void Update() {
-        float delta = Time.deltaTime;
-        SpriteRenderer.sprite = Sprites[(int) Math.Floor(i)];
-        if (shouldChange) {
-            if (shouldOpen) {
-                i += delta * 3;
-                if (i >= Sprites.Length-1) {
-                    open = true;
-                    shouldChange = false;
-                    i = Sprites.Length-1;
-                }
-            } else {
-                i -= delta * 3;
-                if (i <= 0) {
-                    open = false;
-                    shouldOpen = false;
-                    i = 0;
-                }
-            }
-            SpriteRenderer.sprite = Sprites[(int) Math.Floor(i)];
-            rigidbody2D.simulated = !open || !shouldOpen;
-        }
+
+    private float timer = 0;
+
+    private void Update()
+    {
+        shouldOpen = Network.IsOn;
+
+        timer += Time.deltaTime * speed * (shouldOpen ? 1 : -1);
+        timer = Mathf.Clamp(timer, 0, 1);
+
+        var index = (int)(timer * (Sprites.Length - 1));
+        SpriteRenderer.sprite = Sprites[index];
+
+        rigidbody2D.simulated = index != Sprites.Length - 1;
     }
 }
--- Wargames2024/Assets/Source/PressurePlate.cs
+++ Wargames2024/Assets/Source/PressurePlate.cs
@@ -1,35 +1,24 @@
-using System.Collections;
-using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
-using UnityEngine.InputSystem;
 
-public class PressurePlate : MonoBehaviour {
-    public Door door;
-    // Start is called before the first frame update
-    int count = 0;
-    void Start() {
-        
-    }
+public class PressurePlate : MonoBehaviour
+{
+    public WireNetwork Network;
+    private int count = 0;
 
-    // Update is called once per frame
-    void Update() {
-        
-    }
-
-    void OnTriggerEnter2D(Collider2D col) {
-        print("Triggered!");
-        door.shouldOpen = true;
-        door.shouldChange = true;
+    private void OnTriggerEnter2D(Collider2D col)
+    {
         count++;
+
+        Network.EnergySources.Add(gameObject);
     }
 
-    void OnTriggerExit2D(Collider2D col) {
-        print("Exit!");
+    private void OnTriggerExit2D(Collider2D col)
+    {
         count--;
-        if (count == 0) {
-            door.shouldOpen = false;
-            door.shouldChange = true;
+
+        if (count == 0)
+        {
+            Network.EnergySources.Remove(gameObject);
         }
     }
 }
