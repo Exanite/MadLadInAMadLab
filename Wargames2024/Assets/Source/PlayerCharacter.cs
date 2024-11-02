cf500b71eefe483b814b26036572a97faa134a35
Exanite <exanite78012@gmail.com>
2024-11-01 20:57:50 -0400

    Implement player movement

--- Wargames2024/Assets/Source/PlayerCharacter.cs
+++ Wargames2024/Assets/Source/PlayerCharacter.cs
@@ -1,6 +1,22 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerCharacter : MonoBehaviour
 {
+    [Header("Dependencies")]
+    public InputActionReference MovementInput;
+    public Rigidbody2D Rigidbody;
+
     public float MovementSpeed = 10;
+    public float MovementSmoothTime = 0.3f;
+
+    private Vector3 referenceVelocity;
+
+    private void Update()
+    {
+        var targetVelocity = MovementInput.action.ReadValue<Vector2>();
+        targetVelocity *= MovementSpeed;
+
+        Rigidbody.velocity = Vector3.SmoothDamp(Rigidbody.velocity, targetVelocity, ref referenceVelocity, MovementSmoothTime);
+    }
 }
