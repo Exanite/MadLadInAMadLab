6b9afc37446665640bb3f665876e78a7cb802155
Exanite <exanite78012@gmail.com>
2024-11-03 01:13:41 -0500

    Work on level select generator

--- Wargames2024/Assets/Source/LevelSelectGenerator.cs
+++ Wargames2024/Assets/Source/LevelSelectGenerator.cs
@@ -1,7 +1,30 @@
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class LevelSelectGenerator : MonoBehaviour
 {
-    public GameObject WallPrefab;
-    public LevelTeleporter Teleporter;
+    public GameObject EndWall;
+    [FormerlySerializedAs("Teleporter")]
+    public LevelTeleporter TeleporterPrefab;
+    public Transform FirstTeleporterPosition;
+
+    public int EndWallOffset = 10;
+    public int SpaceBetweenTeleporters = 10;
+
+    private void Start()
+    {
+        var levels = GameContext.Instance.GameLevelOrder.Levels;
+
+        // Skip 0 because 0 is level select
+        for (var i = 1; i < levels.Count; i++)
+        {
+            var position = FirstTeleporterPosition.transform.position + i * SpaceBetweenTeleporters * Vector3.right;
+            var level = levels[i];
+
+            var teleporter = Instantiate(TeleporterPrefab, position, Quaternion.identity);
+            teleporter.Initialize(true, i, level);
+        }
+
+        var endWallPosition = EndWall.transform.position;
+    }
 }
