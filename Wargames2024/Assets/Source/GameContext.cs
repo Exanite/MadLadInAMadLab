75636f8f9d7c71bdc271c2ed2ac646f581b3c8be
Exanite <exanite78012@gmail.com>
2024-11-03 01:30:14 -0500

    Move icon references to GameContext

--- Wargames2024/Assets/Source/GameContext.cs
+++ Wargames2024/Assets/Source/GameContext.cs
@@ -1,12 +1,15 @@
 using System;
 using Exanite.Core.Components;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameContext : SingletonBehaviour<GameContext>
 {
     [Header("Dependencies")]
     public FireBehaviour FirePrefab;
     public GameLevelOrder GameLevelOrder;
+    public Image regenIcon;
+    public Image resistIcon;
 
     [NonSerialized]
     public PlayerCharacter Player;
