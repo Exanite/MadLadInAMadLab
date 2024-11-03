38ef34377cf5c8d3315256f1d3e7ab5c31dd7875
Exanite <exanite78012@gmail.com>
2024-11-03 06:24:27 -0500

    Reduce NRE spam when levels are not registered

--- Wargames2024/Assets/Source/UserInterface/LevelNameDisplay.cs
+++ Wargames2024/Assets/Source/UserInterface/LevelNameDisplay.cs
@@ -1,10 +1,35 @@
 using System;
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LevelNameDisplay : MonoBehaviour
 {
     public TMP_Text Text;
+
+    private string sceneText = "";
+
+    private void OnEnable()
+    {
+        SceneManager.sceneLoaded += OnSceneLoaded;
+        UpdateSceneText();
+    }
+
+    private void OnDisable()
+    {
+        SceneManager.sceneLoaded -= OnSceneLoaded;
+    }
+
+    private void OnSceneLoaded(Scene scene, LoadSceneMode loadSceneMode)
+    {
+        UpdateSceneText();
+    }
+
+    private void UpdateSceneText()
+    {
+        var currentScene = GameContext.Instance.GameLevelOrder.GetCurrentLevelInfo();
+        sceneText = $"\nLevel {currentScene.Index} - {currentScene.Info.DisplayName}";
+    }
 
     private void Update()
     {
@@ -12,6 +37,6 @@ public class LevelNameDisplay : MonoBehaviour
         Text.text =
             $"Level: {TimeSpan.FromSeconds(GameContext.Instance.LevelTimer):g} {(GameContext.Instance.IsLevelTimerPaused ? "(Paused)" : "")}" +
             $"\nTotal: {TimeSpan.FromSeconds(GameContext.Instance.GameTimer):g} {(GameContext.Instance.IsGameTimerPaused ? "(Paused)" : "")}" +
-            $"\nLevel {currentScene.Index} - {currentScene.Info.DisplayName}";
+            $"{sceneText}";
     }
 }
