7992f954747533c8ff95787d54a1df25c5792751
LukeGorman321 <lukegorman321@gmail.com>
2024-11-03 21:54:18 -0500

    Edited Level 3 and Race Level based on play testing, Acid Barrel adjustment

--- Wargames2024/Assets/Source/AcidBarrel.cs
+++ Wargames2024/Assets/Source/AcidBarrel.cs
@@ -5,9 +5,9 @@ using UnityEngine;
 public class AcidBarrel : MonoBehaviour {
     public Rigidbody2D Rigidbody;
     private Vector2 prevVelocity;
-    public float threshold = 0.01f;
+    public float threshold = 500f;
     void Update() {
-        if (Math.Abs((prevVelocity - Rigidbody.velocity).magnitude) * Time.deltaTime > threshold) {
+        if ((prevVelocity - Rigidbody.velocity).magnitude > threshold * Time.deltaTime) {
             Instantiate(GameContext.Instance.AcidPrefab, transform.position, Quaternion.identity);
         }
         prevVelocity = Rigidbody.velocity;
